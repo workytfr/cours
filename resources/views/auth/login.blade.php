2f597f54c575ce491ede6978c6197883eb586ea0
Natoons <nadir.tounsi@epitech.eu>
2024-02-24 22:41:25 +0100

    Translate forms login an register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -28,14 +28,14 @@
             <div class="block mt-4">
                 <label for="remember_me" class="flex items-center">
                     <x-checkbox id="remember_me" name="remember" />
-                    <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                    <span class="ms-2 text-sm text-gray-600">{{ __('Se souvenir de moi') }}</span>
                 </label>
             </div>
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
+                        {{ __('Mot de passe oublié?') }}
                     </a>
                 @endif
 
